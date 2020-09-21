a7453755dff7aefa9cb5e7528d9c59280944ce70
Alan Killian <akillian@outlook.com>
2020-09-21 18:10:26 -0400

    commit

--- src/modules/troubleshooter.js
+++ src/modules/troubleshooter.js
@@ -1,76 +1,85 @@
-const wifi_TS = `<h1 style="color:red"><u>iPhone Tips</u></h1>
-<ul>
-    <li>Toggle Wi-Fi</li>
-    <li>If that doesn’ change a thing, toggle Airplane mode</li>
-</ul>
+const wifi_TS = `<h1><u>iPhone Tips</u></h1>
 <div>
-    <section></section>
-    <div>
-        <div></div>
-        <p>Smartphones seem to be lifeless without internet. There cannot be anything more frustrating than
-            problematic WiFi. At times, things may look perfectly fine with and even your WiFi maybe showing a
-            strong signal but your apps don’t work. There are different types of WiFi issues that one could face
-            such as the <a href="https://www.technobezz.com/how-to-fix-iphone-5-wifi-issues/"
-                data-wpel-link="internal">connection&nbsp;that keeps dropping, slow WiFi</a>, unable to connect to
-            WiFi or WiFi settings greyed out. Here are some ways on how to fix iPhone X WiFi issues.</p>
-        <p>First, try the following:</p>
-        <ul>
-            <li>Toggle Wi-Fi</li>
-            <li>If that doesn’ change a thing, toggle Airplane mode</li>
-        </ul>
-        <p>Read also- <a href="https://www.technobezz.com/reset-face-id-iphone-x/" data-wpel-link="internal">How To
-                Reset Face ID On iPhone X</a></p>
-        <p>&nbsp;</p>
-        <div>
-            <div data-src="https://www.youtube.com/embed/-e4rc3liCvw" data-id="-e4rc3liCvw"
-                data-query="feature=oembed">
-                <div data-id="-e4rc3liCvw" data-query="feature=oembed"
-                    data-src="https://www.youtube.com/embed/-e4rc3liCvw"><img loading="lazy" alt=""
-                        src="https://i.ytimg.com/vi/-e4rc3liCvw/hqdefault.jpg" data-ll-status="loaded" width="480"
-                        height="360"><noscript><img src="https://i.ytimg.com/vi/ID/hqdefault.jpg" alt="" width="480"
-                            height="360"></noscript>
-                    <div></div>
-                </div>
-            </div><noscript><iframe title="How To Fix iPhone X Wi-Fi Issues" width="1200" height="675"
-                    src="https://www.youtube.com/embed/-e4rc3liCvw?feature=oembed" frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe></noscript>
-        </div>
-        <h2 id="methods-to-fix-iphone-x-wi-fi-issues"><strong>Methods To Fix iPhone X Wi-Fi Issues</strong></h2>
-        <h3 id="reset-network-settings"><strong>Reset Network Settings </strong></h3>
-        <p>Besides just clearing the network information and DHCP settings this step will also flush out caches.
-            Most WiFi problems are fixed with this simple process. Here is how you can reset your network settings
-            for iPhone X.</p>
-        <p>Settings &gt; General &gt; Reset &gt; Reset Network Settings – tap on this option.</p>
-        <h3 id="turn-off-wifi-networking-services"><strong>Turn Off WiFi Networking Services </strong></h3>
-        <p>This step has also helped several users to solve the WiFi issues. For this you need to go to:</p>
-        <p>Settings &gt; Privacy &gt; Location Services &gt; System Services &gt; WiFi Networking – turn this option
-            off.</p>
-        <p>WiFi networking is not linked to WiFi and is only related to location services. Your WiFi will not be
-            affected if you turn it off.</p>
-        <h3 id="force-restart"><strong>Force Restart </strong></h3>
-        <p>A force restart helps in solving the problem of a dropping connection and solves the iPhone X Wi-Fi
-            issues. Apple has changed the way to force or hard restart <a
-                href="https://www.technobezz.com/common-iphone-8-problems-fixes/"
-                data-wpel-link="internal">iPhone</a> X.&nbsp; Here is what you need to do:</p>
-        <ul>
-            <li>Press the Volume Up button and release</li>
-            <li>Press the Volume Down button and release</li>
-            <li>Now press and hold the Side/Sleep/Wake button and wait till you see the Apple logo before you let
-                the button go.</li>
-        </ul>
-        <p>The Volume Up and Volume Down buttons should be pressed and released quickly.</p>
-        <h3 id="forget-the-wi-fi-network-and-connect-again"><strong>Forget the Wi-Fi Network and Connect Again
-            </strong></h3>
-        <p>If you get continuous prompts which says that you have input a wrong password then you should try
-            forgetting the network and then join it back once again. Follow the steps:</p>
-        <ul>
-            <li>Settings &gt; WiFi &gt; Tap WiFi and select Forget This Network when you get the prompt</li>
-            <li>Exit the screen and wait for 10-20 seconds. Now head back to</li>
-            <li>Settings &gt; WiFi &gt; Select the network and then enter the password &gt; Join the network</li>
-        </ul>
-        <p>This may solve your WiFi problems.
-    </div>`;
+  <p>Here are some ways on how to fix iPhone X WiFi issues.</p>
+  <p>First, try the following:</p>
+  <ul>
+    <li>Toggle Wi-Fi <b><em>Off/On</em></b></li>
+    <li>If that doesn’ change a thing, toggle <b>Airplane mode</b></li>
+  </ul>
+  <p>&nbsp;</p>
+  <h3><strong>Forget the Wi-Fi Network and Connect Again </strong></h3>
+  <p>
+    If you get continuous prompts which says that you have input a wrong
+    password then you should try forgetting the network and then join it
+    back once again. Follow the steps:
+  </p>
+  <ul>
+    <li>
+      <em>Settings</em> &gt; <em>WiFi</em> &gt; Tap <em>WiFi</em> and select <b>Forget This Network</b> when
+      you get the prompt
+    </li>
+    <li>Exit the screen and wait for 10-20 seconds. Now head back to</li>
+    <li>
+      <em>Settings</em> &gt; <em>WiFi</em> &gt; Select the <em><b>BYOD</b></em> network and then enter your 
+      school password
+      &gt; <b>Join</b> the network
+    </li>
+  </ul>
+
+  <h2>
+    <strong>Some Additional Methods To Fix iPhone X Wi-Fi Issues</strong>
+  </h2>
+  <h3><strong>Reset Network Settings </strong></h3>
+  <p>
+    Besides just clearing the network information and DHCP settings this
+    step will also flush out caches. Most WiFi problems are fixed with this
+    simple process. Here is how you can reset your network settings for
+    iPhone X.
+  </p>
+  <p>
+    <em>Settings</em> &gt; <em>General</em> &gt; <em>Reset</em> &gt; <em><u>Reset Network Settings</u></em> – tap on
+    this option.
+  </p>
+  <h3><strong>Turn Off WiFi Networking Services </strong></h3>
+  <p>
+    This step has also helped several users to solve the WiFi issues. For
+    this you need to go to:
+  </p>
+  <p>
+    <em>Settings</em> &gt; <em>Privacy</em> &gt; <em>Location Services</em> &gt; <em>System Services</em> &gt;
+    <em>WiFi Networking</em> – turn this option off.
+  </p>
+  <p>
+    WiFi networking is not linked to WiFi and is only related to location
+    services. Your WiFi will not be affected if you turn it off.
+  </p>
+  <h3><strong>Force Restart </strong></h3>
+  <p>
+    A force restart helps in solving the problem of a dropping connection
+    and solves the iPhone X Wi-Fi issues. Apple has changed the way to force
+    or hard restart
+    <a
+      href="https://www.technobezz.com/common-iphone-8-problems-fixes/"
+      data-wpel-link="internal"
+      >iPhone</a
+    >
+    X.&nbsp; Here is what you need to do:
+  </p>
+  <ul>
+    <li>Press the Volume Up button and release</li>
+    <li>Press the Volume Down button and release</li>
+    <li>
+      Now press and hold the Side/Sleep/Wake button and wait till you see
+      the Apple logo before you let the button go.
+    </li>
+  </ul>
+  <p>
+    The Volume Up and Volume Down buttons should be pressed and released
+    quickly.
+  </p>
+
+  <p>This may solve your WiFi problems.</p>
+</div>`;
 
 const Troubleshooter = {
   wifi: wifi_TS,
